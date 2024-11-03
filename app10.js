75d409bc1a1c9488b2f80fc23e192384cc687c65
hamdan.ga <hamdanchitari@gmail.com>
2024-11-04 01:48:43 +0300

    ha

---
+++ app10.js
@@ -0,0 +1,18 @@
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 10 Result:', calculateGrade(85));
